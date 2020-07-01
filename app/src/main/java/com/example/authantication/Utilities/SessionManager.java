9c98381edad5f871769932d80dd30ed5d82ccbdf
Haider <shkhaider2015@gmail.com>
2020-07-01 19:27:20 +0500

    Send Data Successfully

--- app/src/main/java/com/example/authantication/Utilities/SessionManager.java
+++ app/src/main/java/com/example/authantication/Utilities/SessionManager.java
@@ -5,13 +5,34 @@ import android.content.SharedPreferences;
 
 import com.example.authantication.R;
 
+
 public class SessionManager {
 
-    private SharedPreferences sharedPreferences;
     private Context mCTX;
 
     public SessionManager(Context mCTX) {
         this.mCTX = mCTX;
-        this.sharedPreferences = mCTX.getSharedPreferences(mCTX.getString(R.string.app_name), mCTX.MODE_PRIVATE);
     }
+
+    public void setUserToken(String token)
+    {
+        SharedPreferences sharedPreferences;
+        sharedPreferences = mCTX.getSharedPreferences(mCTX.getString(R.string.app_name), mCTX.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("token", token);
+        editor.apply();
+        editor.commit();
+    }
+
+    public String getUserToken()
+    {
+        SharedPreferences sharedPreferences;
+        sharedPreferences = mCTX.getSharedPreferences(mCTX.getString(R.string.app_name), mCTX.MODE_PRIVATE);
+
+        String token = sharedPreferences.getString("token", "");
+
+        return token;
+    }
+
+
 }
